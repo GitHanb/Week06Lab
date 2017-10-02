a912a0bc343448756b0a26267974d9e4f398786c
636334 <636334@ELF6IT215856.ACDM.DS.SAIT.CA>
2017-10-02 13:31:10 -0600

    Week 04 Lab 1
    created initial files

--- MyLogin/src/java/sait/cprg352/UserService.java
+++ MyLogin/src/java/sait/cprg352/UserService.java
@@ -11,6 +11,33 @@ package sait.cprg352;
  */
 public class UserService {
     
+    private String username;
+    private String password;
+
+    public UserService(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserService() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     public boolean login(String username, String password)
     {
         if((username.equals("adam")|| username.equals("betty")) && password.equals("password"))
