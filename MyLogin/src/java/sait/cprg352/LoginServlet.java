eae43ce0d1c73809d4e08e5b52ea9bd23651d00b
636334 <636334@ELF6IT215856.ACDM.DS.SAIT.CA>
2017-10-02 13:33:58 -0600

    Week 04 Lab 1
    created initial files

--- MyLogin/src/java/sait/cprg352/LoginServlet.java
+++ MyLogin/src/java/sait/cprg352/LoginServlet.java
@@ -6,8 +6,6 @@
 package sait.cprg352;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
